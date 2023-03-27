1652a281e744e7366f6838f60e96f046c457503a
Dmitrii Kim <mitya_kim@icloud.com>
2023-03-26 17:53:07 -0700

    example 2

--- Combine_and_UIKit_ex2/ViewController.swift
+++ Combine_and_UIKit_ex2/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var acceptedSwitch: UISwitch!
+    @IBOutlet weak var privacySwitch: UISwitch!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    // Define publishers
+    @Published private var acceptedTerms = false
+    @Published private var acceptedPrivacy = false
+    @Published private var name = ""
+    
+    // Combine publishers into single stream
+    private var validToSubmit: AnyPublisher<Bool, Never> {
+        return Publishers.CombineLatest3($acceptedTerms, $acceptedPrivacy, $name)
+            .map { terms, privacy, name in
+                return terms && privacy && !name.isEmpty
+            }.eraseToAnyPublisher()
+    }
+    
+    // Define subscriber
+    private var buttonSubscriber: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+        
+        // Hook subscriber up to publisher
+        buttonSubscriber = validToSubmit
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: submitButton)
     }
-
-
+    
+    @IBAction func acceptTerms(_ sender: UISwitch) {
+        acceptedTerms = sender.isOn
+    }
+    
+    @IBAction func acceptPrivacy(_ sender: UISwitch) {
+        acceptedPrivacy = sender.isOn
+    }
+    
+    @IBAction func nameChanged(_ sender: UITextField) {
+        name = sender.text ?? ""
+    }
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+        
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
